cd989afd012a563863667a6b226a9f7ee5ccb84e
ChoInhwa <ihnhwa8362@naver.com>
2023-08-17 17:59:08 +0900

    230817

---
+++ 18258_큐_2.py
@@ -0,0 +1,96 @@
+import sys; input = sys.stdin.readline
+
+# 풀이 3. front, rear
+
+q = [0] * 2_000_000
+front = rear = -1
+
+N = int(input())
+for _ in range(N):
+    # c : 명령어 (command)
+    c = input()
+    if c[:4] == 'push':
+        rear += 1
+        q[rear] = int(c[5:])
+    
+    elif c == 'pop':
+        if front < rear:
+            front += 1
+            print(q[front])
+        else: print(-1)
+
+    elif c == 'size':
+        print(rear - front)
+
+    elif c == 'empty':
+        if front == rear: print(1)
+        else: print(0)
+
+    elif c == 'front':
+        if front < rear: print(q[front+1])
+        else: print(-1)
+
+    elif c == 'back':
+        if front < rear: print(q[rear])
+        else: print(-1)
+
+
+
+# # 풀이 2. deque 시간 초과
+# from collections import deque
+# q = deque()
+
+# N = int(input())
+# for _ in range(N):
+#     # c : 명령어 (command)
+#     c = input()
+#     if c[:4] == 'push':
+#         q.append(c[5:])
+    
+#     elif c == 'pop':
+#         if q: print(q.popleft())
+#         else: print(-1)
+
+#     elif c == 'size':
+#         print(len(q))
+
+#     elif c == 'empty':
+#         if q: print(0)
+#         else: print(1)
+
+#     elif c == 'front':
+#         if q: print(q[0])
+#         else: print(-1)
+
+#     elif c == 'back':
+#         if q: print(q[-1])
+#         else: print(-1)
+
+# # 풀이 1. 시간 초과
+# q = []
+
+# N = int(input())
+# for _ in range(N):
+#     # c : 명령어 (command)
+#     c = input()
+#     if c[:4] == 'push':
+#         q.append(c[5:])
+
+#     elif c == 'pop':
+#         if q: print(q.pop(0))
+#         else: print(-1)
+
+#     elif c == 'size':
+#         print(len(q))
+
+#     elif c == 'empty':
+#         if q: print(0)
+#         else: print(1)
+
+#     elif c == 'front':
+#         if q: print(q[0])
+#         else: print(-1)
+
+#     elif c == 'back':
+#         if q: print(q[-1])
+#         else: print(-1)
