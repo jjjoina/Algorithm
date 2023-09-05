fdb76b44f878f337b5aef6b1aa8e66a48d777972
Inhwa Cho <ihnhwa8362@naver.com>
2023-09-05 20:30:25 +0900

    230905

--- yet/1463_1로_만들기.py
+++ yet/1463_1로_만들기.py
@@ -1,5 +1,37 @@
-# 풀이 2.
+# 풀이 3. [정답] [932ms] dp
+import sys; input = sys.stdin.readline
 
+N = int(input())
+dp = [0, 0] + [N+1] * (N-1)    # 초기화
+
+for i in range(1, N+1):
+    for j in [2*i, 3*i, i+1]:
+        if j <= N:
+            dp[j] = min(dp[j], dp[i]+1)
+
+print(dp[N])
+
+
+
+# # 풀이 2. [정답] [796ms] BFS, 바텀업, 가지치기(?)
+# import sys; input = sys.stdin.readline
+# from collections import deque
+
+# N = int(input())
+
+# visited = [0] * (N+1)
+# q = deque()
+# q.append(1)
+# visited[1] = 1
+# while q:
+#     x = q.popleft()
+#     if x == N: break
+#     for nx in [3*x, 2*x, x+1]:
+#         if nx <= N and not visited[nx]:
+#             q.append(nx)
+#             visited[nx] = visited[x] + 1
+
+# print(visited[N] - 1)
 
 
 
