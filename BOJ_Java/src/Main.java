2ae5f92d7cd697f41f935847dc4039cde68ec892
조인화 <ihnhwa8362@naver.com>
2024-02-13 22:29:14 +0900

    240213

---
+++ BOJ_Java/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = Integer.parseInt(st.nextToken());
+        int sub = Integer.parseInt(st.nextToken());
+
+        if ((sum + sub) % 2 == 0 && sum >= sub) System.out.printf("%d %d", (sum + sub) / 2, (sum - sub) / 2);
+        else System.out.print(-1);
+    }
+}
