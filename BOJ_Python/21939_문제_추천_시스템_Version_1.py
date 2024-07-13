62b469be976e6e247464d3f0fd06103d140a488a
조인화 <ihnhwa8362@naver.com>
2024-07-13 20:25:55 +0900

    240713

---
+++ BOJ_Python/21939_문제_추천_시스템_Version_1.py
@@ -0,0 +1,35 @@
+import sys; input = sys.stdin.readline
+import heapq
+
+problems_hard = []
+problems_easy = []
+solved = set()
+levels = [0] * 100001
+
+N = int(input())
+for _ in range(N):
+    P, L = map(int, input().split())
+    heapq.heappush(problems_hard, [-L, -P])
+    heapq.heappush(problems_easy, [L, P])
+    levels[P] = L
+
+M = int(input())
+for _ in range(M):
+    command = input().split()
+
+    if command[0] == 'recommend':
+        problems = problems_hard if command[1] == '1' else problems_easy
+        while (abs(problems[0][1]), abs(problems[0][0])) in solved:
+            heapq.heappop(problems)
+        print(abs(problems[0][1]))
+
+    elif command[0] == 'add':
+        P = int(command[1])
+        L = int(command[2])
+        heapq.heappush(problems_hard, [-L, -P])
+        heapq.heappush(problems_easy, [L, P])
+        levels[P] = L
+
+    else:
+        P = int(command[1])
+        solved.add((P, levels[P]))
