a28dd0078fb01a955cedbe3e15d3635f4d2ba1e3
조인화 <ihnhwa8362@naver.com>
2024-07-26 20:51:48 +0900

    240726

--- BOJ_Java/src/BOJ2470.java
+++ BOJ_Java/src/BOJ2470.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class BOJ2470 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
