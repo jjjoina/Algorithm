714d4ca6ef110426104fffd71c045664c939bd03
조인화 <ihnhwa8362@naver.com>
2024-09-09 15:28:48 +0900

    240909

--- BOJ_Python/6987_월드컵.py
+++ BOJ_Python/6987_월드컵.py
@@ -11,17 +11,17 @@ def back_tracking(team, opponent):
     for wdl in range(3):    # win-draw-lose
         opponent_wdl = 2 - wdl
 
-        tmp_result[team][wdl] += 1
-        tmp_result[opponent][opponent_wdl] += 1
+        result[team][wdl] -= 1
+        result[opponent][opponent_wdl] -= 1
 
-        if tmp_result[team][wdl] <= result[team][wdl] and tmp_result[opponent][opponent_wdl] <= result[opponent][opponent_wdl]:
+        if result[team][wdl] >= 0 and result[opponent][opponent_wdl] >= 0:
             if opponent == 5:
                 back_tracking(team + 1, team + 2)
             else:
                 back_tracking(team, opponent + 1)
 
-        tmp_result[team][wdl] -= 1
-        tmp_result[opponent][opponent_wdl] -= 1
+        result[team][wdl] += 1
+        result[opponent][opponent_wdl] += 1
 
 
 def check():
@@ -38,7 +38,6 @@ for _ in range(4):
     lst = list(map(int, input().split()))
 
     result = [lst[i:i + 3] for i in range(0, 18, 3)]
-    tmp_result = [[0] * 3 for _ in range(6)]
     ans = 0
 
     check()
